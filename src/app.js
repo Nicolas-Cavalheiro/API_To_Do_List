d1cfea67c5fb475a84ef8e32443f74487d304429
Nicolas-Cavalheiro <nicolascps.cavalheiro@gmail.com>
2026-04-08 18:02:18 -0300

    Segundo commit

--- src/app.js
+++ src/app.js
@@ -1,1 +1,24 @@
 //arquivo apps
+
+const http = require('http');
+
+//importa as rotas
+const taskRoutes = require ('./routes/tasks');
+
+//cria um servidor
+const server = http.createServer((req, res) => {
+
+    //define cabeçalho JSON
+    res.setHeader('Content-Type', 'application/json');
+
+    //chama o roteador
+    taskRoutes(req, res);
+});
+
+//porta
+const PORT = 3000;
+
+//inicia o servidor
+server.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+})
--- src/controllers/taskControllers.js
+++ src/controllers/taskControllers.js
@@ -1,1 +1,59 @@
 //arquivo controller
+
+const taskService = require ('../services/taskServices');
+
+//função auxiliar para ler body
+const getRequestBody = (req) => {
+    return new Promise((resolve, reject) => {
+        let body = '';
+
+        req.on('data', chunk => {
+            body += chunk.toString();
+        });
+        
+        req.on('end', () => {
+            resolve(JSON.parse(body));
+        });
+    });
+};
+
+//criar tarefa
+const createTask = async (req,res) => {
+    const body = await getRequesteBody (req);
+
+    const task = taskService.addTask(body.title);
+
+    req.statusCode = 201;
+    res.end(JSON.stringify(task));
+};
+
+//listar tarefas
+const listTasks = (req, res) => {
+    const tasks = taskService.getTasks();
+
+    res.statusCode = 200;
+    res.end(JSON.stringify(tasks));
+};
+
+//atualizar tarefa
+const updateTask = async (req, res, id) => {
+    const body = await getRequestBody(req);
+
+    const task = taskService.updateTask(id, body.title);
+
+    if(!task){
+        res.statusCode = 404;
+        return res.end (JSON.stringify(
+            { message: 'Não encontrada' }
+        ));
+    }
+
+    res.end(JSON.stringify({ messsage: 'Removida'}));
+};
+
+module.exports = {
+    createTask,
+    listTasks,
+    updateTask,
+    deleteTask
+}
